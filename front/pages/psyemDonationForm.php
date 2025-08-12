c93074a28491e9a0fa2c1564055ce2c4c7592bd3
Zuber7489 <zs808081@gmail.com>
2025-08-12 11:55:01 +0530

    update chinese lang fixed

--- front/pages/psyemDonationForm.php
+++ front/pages/psyemDonationForm.php
@@ -1,4 +1,30 @@
-<?php ob_start(); ?>
+<?php 
+ob_start(); 
+
+// Multiple ways to detect Chinese language
+$current_locale = get_locale();
+$is_chinese = false;
+
+// Method 1: Check WordPress locale
+if ($current_locale === 'zh_HK' || $current_locale === 'zh_CN' || $current_locale === 'zh-HK' || $current_locale === 'zh-CN') {
+    $is_chinese = true;
+}
+
+// Method 2: Check URL parameters
+if (isset($_GET['lang']) && ($_GET['lang'] === 'zh' || $_GET['lang'] === 'zh_HK' || $_GET['lang'] === 'zh-HK')) {
+    $is_chinese = true;
+}
+
+// Method 3: Check if URL contains /zh/
+if (strpos($_SERVER['REQUEST_URI'], '/zh/') !== false || strpos($_SERVER['REQUEST_URI'], '/chinese/') !== false) {
+    $is_chinese = true;
+}
+
+// Method 4: Check referer URL for language
+if (isset($_SERVER['HTTP_REFERER']) && (strpos($_SERVER['HTTP_REFERER'], '/zh/') !== false || strpos($_SERVER['HTTP_REFERER'], 'lang=zh') !== false)) {
+    $is_chinese = true;
+}
+?>
 <style>
     .sponsor-call-to-action::after {
         background: url(<?= PSYEM_ASSETS . '/images/more-arrow.svg' ?>) no-repeat center;
@@ -21,35 +47,17 @@
             <div class="row">
                 <div class="col-md-12 col-lg-6">
                     <div class="content">
-                        <h1 class="title"> <?= __('Make a Donation', 'psyeventsmanager') ?> </h1>
+                        <h1 class="title"> <?php
+                            if ($is_chinese) {
+                                echo '捐款';
+                            } else {
+                                _e('Make a Donation', 'psyeventsmanager');
+                            }
+                            ?> </h1>
                         <p>
                             <?php
-                            // Multiple ways to detect Chinese language
-                            $current_locale = get_locale();
-                            $is_chinese = false;
-                            
-                            // Method 1: Check WordPress locale
-                            if ($current_locale === 'zh_HK' || $current_locale === 'zh_CN' || $current_locale === 'zh-HK' || $current_locale === 'zh-CN') {
-                                $is_chinese = true;
-                            }
-                            
-                            // Method 2: Check URL parameters
-                            if (isset($_GET['lang']) && ($_GET['lang'] === 'zh' || $_GET['lang'] === 'zh_HK' || $_GET['lang'] === 'zh-HK')) {
-                                $is_chinese = true;
-                            }
-                            
-                            // Method 3: Check if URL contains /zh/
-                            if (strpos($_SERVER['REQUEST_URI'], '/zh/') !== false || strpos($_SERVER['REQUEST_URI'], '/chinese/') !== false) {
-                                $is_chinese = true;
-                            }
-                            
-                            // Method 4: Check referer URL for language
-                            if (isset($_SERVER['HTTP_REFERER']) && (strpos($_SERVER['HTTP_REFERER'], '/zh/') !== false || strpos($_SERVER['HTTP_REFERER'], 'lang=zh') !== false)) {
-                                $is_chinese = true;
-                            }
-                            
                             if ($is_chinese) {
-                                echo '您的捐款將大大有助於拯救生命，支持我們在香港推廣婦科癌症的預防、早期檢測及 最佳治療的工作。';
+                                echo '你的捐款將有助於拯救生命, 支援我們的努力, 促進預防, 早期發現和最佳治療婦科癌症。';
                             } else {
                                 echo 'Your donation will go a long way in helping to save lives by supporting our efforts to promote prevention, early detection, and optimal treatment of gynaecological cancer in Hong Kong.';
                             }
@@ -58,7 +66,7 @@
                         <p>
                             <?php
                             if ($is_chinese) {
-                                echo '我們在美國和香港都是註冊的慈善機構，並能提供兩地的免稅收據。';
+                                echo '我們是香港的註冊慈善機構，可以為司法管轄區提供稅收抵扣收據。';
                             } else {
                                 echo 'We are a registered charity in both the US and Hong Kong and are able to provide tax deductible receipts for both jurisdictions.';
                             }
@@ -71,23 +79,59 @@
                 <div class="col-md-12 col-lg-6 posRealtive">
                     <div class="right_section">
                         <div class="donation_type monthly">
-                            <h2> <?= __('Monthly Donation', 'psyeventsmanager') ?></h2>
-                            <p> <?= __('Help keep our programs running', 'psyeventsmanager') ?></p>
+                            <h2> <?php
+                            if ($is_chinese) {
+                                echo '每月定額捐款';
+                            } else {
+                                _e('Monthly Donation', 'psyeventsmanager');
+                            }
+                            ?></h2>
+                            <p> <?php
+                            if ($is_chinese) {
+                                echo '幫助維持我們的計劃運行';
+                            } else {
+                                _e('Help keep our programs running', 'psyeventsmanager');
+                            }
+                            ?></p>
                             <a href="javascript:void(0);"
                                 data-toggle="modal"
                                 data-target="#lab-slide-bottom-popup"
                                 class="donation-call-to-action psyemMonthlyDonationElm"
-                                data-donation-title="<?= __('Monthly Donation', 'psyeventsmanager') ?>">
-                                <?= __('Select Amount', 'psyeventsmanager') ?>
+                                data-donation-title="<?php echo $is_chinese ? '每月定額捐款' : __('Monthly Donation', 'psyeventsmanager'); ?>">
+                                <?php
+                            if ($is_chinese) {
+                                echo '選擇金額';
+                            } else {
+                                _e('Select Amount', 'psyeventsmanager');
+                            }
+                            ?>
                             </a>
                         </div>
                         <div class="donation_type onetime">
-                            <h2> <?= __('One Time Donation', 'psyeventsmanager') ?></h2>
-                            <p><?= __('Every penny counts', 'psyeventsmanager') ?></p>
+                            <h2> <?php
+                            if ($is_chinese) {
+                                echo '單次捐款';
+                            } else {
+                                _e('One Time Donation', 'psyeventsmanager');
+                            }
+                            ?></h2>
+                            <p><?php
+                            if ($is_chinese) {
+                                echo '一分一毫同樣重要';
+                            } else {
+                                _e('Every penny counts', 'psyeventsmanager');
+                            }
+                            ?></p>
                             <a href="javascript:void(0);"
                                 class="donation-call-to-action psyemOnetimeDonationElm"
-                                data-donation-title="<?= __('One Time Donation', 'psyeventsmanager') ?>">
-                                <?= __('Select Amount', 'psyeventsmanager') ?>
+                                data-donation-title="<?php echo $is_chinese ? '單次捐款' : __('One Time Donation', 'psyeventsmanager'); ?>">
+                                <?php
+                            if ($is_chinese) {
+                                echo '選擇金額';
+                            } else {
+                                _e('Select Amount', 'psyeventsmanager');
+                            }
+                            ?>
                             </a>
                         </div>
                         <div class="donation_type sponsorship">
@@ -98,10 +142,22 @@
                                 _e('Donate towards our Core Objectives', 'psyeventsmanager');
                             }
                             ?></h2>
-                            <p><?= __('Make a targeted impact', 'psyeventsmanager') ?></p>
+                            <p><?php
+                            if ($is_chinese) {
+                                echo '產生針對性影響';
+                            } else {
+                                _e('Make a targeted impact', 'psyeventsmanager');
+                            }
+                            ?></p>
                             <a href="<?= $onetime_donation_page_link ?>"
                                 class="sponsor-call-to-action">
-                                <?= __('Areas of Impact', 'psyeventsmanager') ?>
+                                <?php
+                            if ($is_chinese) {
+                                echo '更多資訊';
+                            } else {
+                                _e('Areas of Impact', 'psyeventsmanager');
+                            }
+                            ?>
                             </a>
                         </div>
                     </div>
